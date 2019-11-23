f726ddb2fd9a13adb4be04827e78ece5ba987be6
Thomas Gredig <thomas.gredig@csulb.edu>
2019-11-23 12:18:55 -0800

    allow case if no deposition was made

--- R/NTE.plotDeposition.R
+++ R/NTE.plotDeposition.R
@@ -9,53 +9,56 @@
 #' @export
 NTE.plotDeposition <- function(d, buffer=0.5) {
   d2 = NTE.depStartEnd(d, buffer)
-  deposition.start = d2$time.sec[1]/3600
-  deposition.end = d2$time.sec[nrow(d2)]/3600
-  Tdep.max = mean(d2$T.substrate) + sd(d2$T.substrate)
-  Tdep.min = mean(d2$T.substrate) - sd(d2$T.substrate)
-  p.max = (mean(d2$pressure) + sd(d2$pressure))*1e6
-  p.min = (mean(d2$pressure) - sd(d2$pressure))*1e6
-  rate.max = (mean(d2$deposition.rate) + sd(d2$deposition.rate))
-  rate.min = (mean(d2$deposition.rate) - sd(d2$deposition.rate))
+  p.sum = NA
+  if (!is.na(d2)) {
+    deposition.start = d2$time.sec[1]/3600
+    deposition.end = d2$time.sec[nrow(d2)]/3600
+    Tdep.max = mean(d2$T.substrate) + sd(d2$T.substrate)
+    Tdep.min = mean(d2$T.substrate) - sd(d2$T.substrate)
+    p.max = (mean(d2$pressure) + sd(d2$pressure))*1e6
+    p.min = (mean(d2$pressure) - sd(d2$pressure))*1e6
+    rate.max = (mean(d2$deposition.rate) + sd(d2$deposition.rate))
+    rate.min = (mean(d2$deposition.rate) - sd(d2$deposition.rate))
 
-  p1 = ggplot(d2, aes(time.sec/3600,pressure*1e6)) +
-    geom_rect(aes(xmin=deposition.start, xmax=deposition.end, ymin=-Inf, ymax=Inf),fill='grey90',alpha=0.1, color='grey') +
-    geom_rect(aes(xmin=deposition.start, xmax=deposition.end, ymin=p.max, ymax=p.min),
-              fill='orange',alpha=1, color='orange') +
-    geom_path(size=2, col='red') +
-    xlab('time (h)') +
-    ylab(expression(paste('p (',mu,'Torr)'))) +
-    ggtitle(paste('pressure:',signif(mean(d2$pressure),2),'Torr')) +
-    theme_bw(base_size = 8)
+    p1 = ggplot(d2, aes(time.sec/3600,pressure*1e6)) +
+      geom_rect(aes(xmin=deposition.start, xmax=deposition.end, ymin=-Inf, ymax=Inf),fill='grey90',alpha=0.1, color='grey') +
+      geom_rect(aes(xmin=deposition.start, xmax=deposition.end, ymin=p.max, ymax=p.min),
+                fill='orange',alpha=1, color='orange') +
+      geom_path(size=2, col='red') +
+      xlab('time (h)') +
+      ylab(expression(paste('p (',mu,'Torr)'))) +
+      ggtitle(paste('pressure:',signif(mean(d2$pressure),2),'Torr')) +
+      theme_bw(base_size = 8)
 
-  p2 = ggplot(d2, aes(time.sec/3600,thickness)) +
-    geom_rect(aes(xmin=deposition.start, xmax=deposition.end, ymin=-Inf, ymax=Inf),fill='grey90',alpha=0.1, color='grey') +
-    geom_path(size=2, col='red') +
-    xlab('time (h)') +
-    ylab(expression(paste('t (m.u.)'))) +
-    ggtitle(paste('thickness:',signif(max(d2$thickness)-min(d2$thickness),2),'m.u.')) +
-    theme_bw(base_size = 8)
+    p2 = ggplot(d2, aes(time.sec/3600,thickness)) +
+      geom_rect(aes(xmin=deposition.start, xmax=deposition.end, ymin=-Inf, ymax=Inf),fill='grey90',alpha=0.1, color='grey') +
+      geom_path(size=2, col='red') +
+      xlab('time (h)') +
+      ylab(expression(paste('t (m.u.)'))) +
+      ggtitle(paste('thickness:',signif(max(d2$thickness)-min(d2$thickness),2),'m.u.')) +
+      theme_bw(base_size = 8)
 
-  p3 = ggplot(d2, aes(time.sec/3600,T.substrate)) +
-    geom_rect(aes(xmin=deposition.start, xmax=deposition.end, ymin=-Inf, ymax=Inf),fill='grey90',alpha=0.1, color='grey') +
-    geom_rect(aes(xmin=deposition.start, xmax=deposition.end, ymin=Tdep.max, ymax=Tdep.min),
-              fill='orange',alpha=1, color='orange') +
-    geom_point(size=2, col='red') +
-    xlab('time (h)') +
-    ylab(expression(paste('T'[sub],' ('^o,'C)'))) +
-    ggtitle(paste('T substrate:',signif(mean(d2$T.substrate),2),'oC')) +
-    theme_bw(base_size = 8)
+    p3 = ggplot(d2, aes(time.sec/3600,T.substrate)) +
+      geom_rect(aes(xmin=deposition.start, xmax=deposition.end, ymin=-Inf, ymax=Inf),fill='grey90',alpha=0.1, color='grey') +
+      geom_rect(aes(xmin=deposition.start, xmax=deposition.end, ymin=Tdep.max, ymax=Tdep.min),
+                fill='orange',alpha=1, color='orange') +
+      geom_point(size=2, col='red') +
+      xlab('time (h)') +
+      ylab(expression(paste('T'[sub],' ('^o,'C)'))) +
+        ggtitle(paste('T substrate:',signif(mean(d2$T.substrate),2),'oC')) +
+        theme_bw(base_size = 8)
 
-  p4 = ggplot(d2, aes(time.sec/3600,deposition.rate)) +
-    geom_rect(aes(xmin=deposition.start, xmax=deposition.end, ymin=-Inf, ymax=Inf),fill='grey90',alpha=0.1, color='grey') +
-    geom_rect(aes(xmin=deposition.start, xmax=deposition.end, ymin=rate.max, ymax=rate.min),
-              fill='orange',alpha=1, color='orange') +
-    geom_point(size=2, col='red') +
-    xlab('time (h)') +
-    ylab(expression(paste('r (m.u./s)'))) +
-    ggtitle(paste('rate:',signif(mean(d2$deposition.rate),2),'m.u./s'))
-  theme_bw(base_size = 8)
+      p4 = ggplot(d2, aes(time.sec/3600,deposition.rate)) +
+        geom_rect(aes(xmin=deposition.start, xmax=deposition.end, ymin=-Inf, ymax=Inf),fill='grey90',alpha=0.1, color='grey') +
+        geom_rect(aes(xmin=deposition.start, xmax=deposition.end, ymin=rate.max, ymax=rate.min),
+                  fill='orange',alpha=1, color='orange') +
+        geom_point(size=2, col='red') +
+        xlab('time (h)') +
+        ylab(expression(paste('r (m.u./s)'))) +
+        ggtitle(paste('rate:',signif(mean(d2$deposition.rate),2),'m.u./s'))
+      theme_bw(base_size = 8)
 
-  p.sum = plot_grid(p1, p2, p3, p4, labels = c('A', 'B', 'C', 'D'), label_size = 12)
+      p.sum = plot_grid(p1, p2, p3, p4, labels = c('A', 'B', 'C', 'D'), label_size = 12)
+  }
   p.sum
 }
